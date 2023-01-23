10e877911607f1a046f574583dec35c98215a63b
Rafael Maia <rafaelmaia3002@gmail.com>
2023-01-23 11:54:12 -0300

    Possible moves of a piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -68,7 +68,7 @@ public class UI {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
             }
             else {
-                System.out.print(ANSI_BLACK + piece + ANSI_RESET);
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
         System.out.print(" ");
